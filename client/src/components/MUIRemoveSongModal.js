f2d0a698bd6e153467b6736cf2f8ab3816d01bd7
Rishith Kyatham <96209369+rkyatham01@users.noreply.github.com>
2022-11-05 02:09:11 -0400

    FinalDone

--- client/src/components/MUIRemoveSongModal.js
+++ client/src/components/MUIRemoveSongModal.js
@@ -51,39 +51,5 @@ export default function MUIRemoveSongModal() {
             <Button onClick={handleCancelRemoveSong}> Cancel </Button>
         </Box>
         </Modal>
-    //     <Modal
-    //         open={store.currentSong !== null}
-    //     >
-    //         <Box sx={style}>
-    //         <div
-    //     id="remove-song-modal"
-    //     className={modalClass}
-    //     data-animation="slideInOutLeft">
-    //     <div className="modal-root" id='verify-remove-song-root'>
-    //         <div className="modal-north">
-    //             Remove {songTitle}?
-    //         </div>
-    //         <div className="modal-center">
-    //             <div className="modal-center-content">
-    //                 Are you sure you wish to permanently remove {songTitle} from the playlist?
-    //             </div>
-    //         </div>
-    //         <div className="modal-south">
-    //             <input type="button" 
-    //                 id="remove-song-confirm-button" 
-    //                 className="modal-button" 
-    //                 onClick={handleConfirmRemoveSong} 
-    //                 value='Confirm' />
-    //             <input 
-    //                 type="button" 
-    //                 id="remove-song-cancel-button" 
-    //                 className="modal-button" 
-    //                 onClick={handleCancelRemoveSong} 
-    //                 value='Cancel' />
-    //         </div>
-    //     </div>
-    // </div>
-    //         </Box>
-    //     </Modal>
     );
 }
